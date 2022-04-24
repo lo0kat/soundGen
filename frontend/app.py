2695c63465670d051e2f7c96528754dc5694c2e4
sonicpierre <pvirgaux@gmail.com>
2022-04-24 12:09:44 +0200

    Mise a jour front back pour test segmentation son enregistre

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,22 @@
 import gradio as gr
 import requests
 import ast
+import numpy as np
+
+
+def reconvertion_request_res(res):
+    """
+    Change the request text into number dictionnary
+    """
+
+    first_trad = ast.literal_eval(res.text)
+    second_trad = {}
+    for i in first_trad:
+        for j in first_trad[i]:
+            second_trad[i] = {int(j):ast.literal_eval(first_trad[i][j])}
+    
+    return second_trad
+
 
 def greet(text1:str, audio1:tuple, text2:str, audio2:tuple):
     #Transform the audio
@@ -8,11 +24,15 @@ def greet(text1:str, audio1:tuple, text2:str, audio2:tuple):
         text1: [str(audio1[0]), str(list(audio1[1]))],
         text2: [str(audio2[0]), str(list(audio2[1]))]
         }
+
     #Send samples to the model
     res = requests.post("http://127.0.0.1:8000/preprocess", json=json_original)
-    dico_res = ast.literal_eval(res.text)
+    res_converted = reconvertion_request_res(res)
+    nb = list(res_converted[text1].keys())[0]
+    song = np.array(res_converted[text1][nb], dtype='int16')
+    sr = 48000
 
-    return "Hello World"
+    return (sr,song)
 
 
 #Permet l'enregistrement du son et sa modification de façon interactive
@@ -22,9 +42,10 @@ text2 = gr.inputs.Textbox(type="str", label="Player 2 Name")
 audio2 = gr.inputs.Audio(source="microphone", label='Enregistrement 2', optional=False)
 iface = gr.Interface(fn=greet,
                     inputs=[text1, audio1, text2, audio2], 
-                    outputs="text",
+                    outputs="audio",
                     title="Who is the fake bird",
                     description="This is made to see who will imitate the best the birds of hese choice.",)
+                    
 
 if __name__ == "__main__":
     app, local_url, share_url = iface.launch()
