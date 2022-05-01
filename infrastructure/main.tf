2f502f8c7246d30bccfd158cda6ae8d6dace66d5
louis <louis.chomel@gmail.com>
2022-05-01 19:24:01 +0200

    fix 3 terraform yaml

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,9 +45,14 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  key_name = "ML_ICC_key" # keypair
+  key_name               = "ML_ICC_key"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y git
+              EOF
 }
 
 resource "aws_security_group" "web-sg" {
