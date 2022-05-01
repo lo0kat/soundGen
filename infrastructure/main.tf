8c66551ff7b48ebfb84ead85f84d9925150c4f02
louis <louis.chomel@gmail.com>
2022-05-01 19:34:37 +0200

    Add keypair and install git on remote instance

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,15 +45,13 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
+  key_name               = "ML_ICC_key"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              apt-get install -y git
               EOF
 }
 
