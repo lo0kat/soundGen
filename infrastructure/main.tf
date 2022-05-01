0ec588297a3e50a1b5d55585dce5e89b1153d350
louis <louis.chomel@gmail.com>
2022-05-01 19:59:19 +0200

    fix main.tf syntax

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -58,8 +58,8 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port = 22
-    to_port = 22
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
