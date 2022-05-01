656e9fd93906fb83f224f2e57f8f30abccc591e4
louis <louis.chomel@gmail.com>
2022-05-01 16:15:08 +0200

    Test SSH to remote

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,16 +45,9 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
+  key_name               = "ML_ICC_key" # keypair
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
 }
 
 resource "aws_security_group" "web-sg" {
@@ -74,6 +67,6 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+output "gpu-instance-address" {
+  value = "${aws_instance.web.public_dns}"
 }
