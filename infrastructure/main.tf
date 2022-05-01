0ef90580b0d15a9d27767389e8e4428b7bffa939
louis <louis.chomel@gmail.com>
2022-05-01 19:30:45 +0200

    revert back to fix tf syntax

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,13 +45,15 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  key_name               = "ML_ICC_key"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
-              apt-get install -y git
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
               EOF
 }
 
@@ -73,5 +75,5 @@ resource "aws_security_group" "web-sg" {
 }
 
 output "gpu-instance-address" {
-  value = "${aws_instance.web.public_dns}"
+  value = aws_instance.web.public_dns
 }
