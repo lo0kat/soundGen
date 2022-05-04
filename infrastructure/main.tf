cda162f6d46d17479ef6b4eed9174815b90f9f2b
louis <louis.chomel@gmail.com>
2022-05-04 17:40:40 +0200

    feat : add iam roles for bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,6 +27,57 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 
+resource "aws_iam_role" "web_iam_role" {
+    name = "web_iam_role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "web_instance_profile" {
+    name = "web_instance_profile"
+    roles = ["web_iam_role"]
+}
+
+resource "aws_iam_role_policy" "web_iam_role_policy" {
+  name = "web_iam_role_policy"
+  role = "${aws_iam_role.web_iam_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::bucket-name"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["arn:aws:s3:::dl-model-bucket-cytech64/*"]
+    }
+  ]
+}
+EOF
+}
+
+
 resource "aws_instance" "training_instance" {
   ami                    = "ami-015ba8cf6eb94ee23"
   instance_type          = "t2.medium"
@@ -38,6 +89,7 @@ resource "aws_instance" "training_instance" {
               apt-get update
               apt-get install -y git 
               EOF
+  iam_instance_profile = "${aws_iam_instance_profile.web_instance_profile.id}"
 }
 
 resource "aws_security_group" "web-sg" {
