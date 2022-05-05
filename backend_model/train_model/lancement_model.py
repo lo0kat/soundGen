800732bc6f17335225859019419572b9d1ea5c9f
sonicpierre <pvirgaux@gmail.com>
2022-05-05 14:53:21 +0200

    Preprocess spectrograms

--- backend_model/train_model/lancement_model.py
+++ backend_model/train_model/lancement_model.py
@@ -103,14 +103,14 @@ def creation_spectrogram(espece : str) -> None:
 
 if __name__ == "__main__":
 
-    data_getter = Data_Recup("~/kaggle.json")
-    data_getter.get_songs()
+    #data_getter = Data_Recup("~/kaggle.json")
+    #data_getter.get_songs()
 
     meta_df = pd.read_csv(config.LIEN_METADATA)
     espece = chargement_espece(meta_df, config.NB_ESPECE)
-    decoupe_son(espece, meta_df)
-    num_supp = sup_enregistrement_court(espece)
-    creation_spectrogram(espece)
+    #decoupe_son(espece, meta_df)
+    #num_supp = sup_enregistrement_court(espece)
+    #creation_spectrogram(espece)
     spec = os.listdir("./preprocessed_data/"+ espece[0] +"/spectrograms")
     taille_input = np.load("./preprocessed_data/"+ espece[0] +"/spectrograms/" + spec[0]).shape
 
@@ -120,4 +120,7 @@ if __name__ == "__main__":
     bird_singer.fit_classique(x_train)
     bird_singer.autoencoder.save("model")
     '''
-    ParameterTuning(config.tuning_dico, taille_input).tune(x_train)
+    
+    param_tuner = ParameterTuning(config.tuning_dico, taille_input)
+    param_tuner.logwandb("W&B.txt")
+    param_tuner.tune(x_train)
