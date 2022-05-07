5084eec5fafd61b99b168d08bddd6122be2d63c6
louis <louis.chomel@gmail.com>
2022-05-07 17:06:56 +0200

    test gpu with different ami

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,7 +80,7 @@ EOF
 
 
 resource "aws_instance" "training_instance" {
-  ami                    = "ami-0cd454db24da5cb9b"
+  ami                    = "ami-015ba8cf6eb94ee23"
   instance_type          = "g4dn.xlarge"
   key_name               = "ML_ICC_key"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
